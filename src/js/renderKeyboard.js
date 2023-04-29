dfecb023e994497917dbd5f5c0768720320a3f6e
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-04-29 18:28:45 +0200

    fix: display correct letter

--- src/js/renderKeyboard.js
+++ src/js/renderKeyboard.js
@@ -3,16 +3,17 @@ export function renderKeyboard(keys) {
 
   keys.forEach((key) => {
     const button = document.createElement("button");
-    button.innerText = key.label;
+    button.innerText = key.keyENG;
 
     button.setAttribute("data-code", key.code);
     button.classList.add("key");
 
     button.addEventListener("click", (event) => {
       const clickedButton = event.target;
-      const key = clickedButton.getAttribute("data-code");
+      const code = clickedButton.getAttribute("data-code");
       const textArea = document.querySelector(".text-area");
-      textArea.value += key.key;
+      const key = keys.find((k) => k.code === code);
+      textArea.value += key.keyENG;
     });
 
     container.appendChild(button);
